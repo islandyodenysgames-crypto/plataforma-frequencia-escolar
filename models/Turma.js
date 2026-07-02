56f42811b7c90c919633406d88d1dc7fd94a24b6
islandyodenysgames-crypto <islandyodenysgames@gmail.com>
2026-07-02 01:15:15 -0300

    Update Turma.js

--- models/Turma.js
+++ models/Turma.js
@@ -6,7 +6,11 @@ const TurmaSchema = new mongoose.Schema({
         required: true,
         unique: true, // Impede turmas com nomes duplicados
         trim: true
+    },
+    fotoUrl: {
+        type: String,
+        default: '' // Armazenará a URL da imagem da turma para o destaque de 1º lugar na TV
     }
-}, { timestamps: true });
+}, { timestamps: true }); // Mantém o controle automático de data de criação e modificação
 
 module.exports = mongoose.model('Turma', TurmaSchema);
